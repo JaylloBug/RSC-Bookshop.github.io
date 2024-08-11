bcd646dc79905db9c95a6dd0faff5ec2478cd08c
JaylloBug <20220424@roytec.edu>
2024-08-11 17:14:04 -0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        name: "Chocolate Digestive",
+        price: "$3.00",
+        image: "https://belairstore.com/wp-content/uploads/2020/09/04003200201.webp",
+        description: "A delicious chocolate digestive biscuit."
+    },
+    {
+        name: "Caramel",
+        price: "$5.00",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1TvxgqVNSlQjamv-foDyGPsV_rqBfSAkojg&s",
+        description: "Rich caramel treat."
+    },
+    {
+        name: "Kiss Holsum Danish's",
+        price: "$6.00",
+        image: "https://www.fittstmarket.com/web/image/product.template/21655/image_1920?unique=0483f2d",
+        description: "Tasty danish pastries."
+    }
+];
